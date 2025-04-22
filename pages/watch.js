981973ec9d144b589768cafbb9649d04430cbe3e
Logan <95306463+LaganYT@users.noreply.github.com>
2025-04-22 11:06:02 -0500

    nocookie embed

--- pages/watch.js
+++ pages/watch.js
@@ -10,7 +10,7 @@ export default function Watch() {
         <iframe
           width="560"
           height="315"
-          src={`https://www.youtube.com/embed/${v}`}
+          src={`https://www.youtube-nocookie.com/embed/${v}`}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
