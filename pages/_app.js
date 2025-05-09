c0317ebb1c8d308753ac15bff68ee7407c86435b
Logan <95306463+LaganYT@users.noreply.github.com>
2025-05-09 10:10:57 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
     <div className="min-h-screen flex flex-col bg-[#181818] text-white font-['Roboto']">
       {/* Navbar */}
       <nav className="bg-gray-900 p-4 flex flex-col sm:flex-row items-center justify-between gap-4 shadow-md border-b border-gray-700">
-        <h1 className="text-2xl font-bold text-red-600">ImpactTube</h1>
+        <Link href="/" className="text-blue-400 hover:underline"><h1 className="text-2xl font-bold text-red-600">ImpactTube</h1></Link>
         <form onSubmit={handleSearch} className="flex items-center gap-2 w-full sm:w-auto">
           <input
             type="text"
@@ -34,9 +34,6 @@ export default function App({ Component, pageProps }) {
             Search
           </button>
         </form>
-        <Link href="/" className="text-blue-400 hover:underline">
-          Home
-        </Link>
       </nav>
 
       {/* Main Page Content */}
